67bfc9d622e0ade8fbeea09f95d9bff0ffa3566a
Misha <boyaxchyan.misha@gmail.com>
2022-12-24 17:30:57 +0300

    IfForexemple

--- src/homwork/Homwork1.java
+++
@@ -1,8 +0,0 @@
-package homwork;
-
-public class Homwork1 {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-
-    }
-}
---
+++ src/homwork1/IfForExample.java
@@ -0,0 +1,38 @@
+package homwork1;
+
+public class IfForExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 40;
+
+        if (a > b) {
+            System.out.println("max " + a);
+        }
+
+        if (b > a) {
+            System.out.println("max " + b);
+        }
+        a = 10;
+        b = 30;
+
+        for (int i = a; i < b; i++) {
+            System.out.println(i);
+
+        }
+
+
+        a = 'a'; {
+            System.out.println("a");
+
+            b = 'b';
+            System.out.println("b");
+
+
+
+            if (a == b);
+            System.out.println(a == b);
+
+
+        }
+    }
+}
